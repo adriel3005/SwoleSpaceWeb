31e8468109913a078d1f1cb865f1284e78acf2fa
adriel3005 <30638262+adriel3005@users.noreply.github.com>
2023-02-10 23:45:16 -0600

    Add basic Register form
    Capture form input in interface
    
    Ref #1

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,27 +1,83 @@
-import React from 'react';
-import logo from '../../Assets/logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  makeStyles,
+  Container,
+  Typography,
+  TextField,
+  Button,
+} from '@material-ui/core'
+import './App.css'
+import { useForm } from 'react-hook-form'
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles(theme => ({
+  heading: {
+    textAlign: 'center',
+    margin: theme.spacing(1, 0, 4),
+  },
+  submitButton: {
+    marginTop: theme.spacing(4),
+  },
+}))
+
+interface IFormInput {
+  email: string
+  firstName: string
+  password: string
 }
 
+function App() {
+  const { register, handleSubmit } = useForm<IFormInput>()
 
-export default App;
+  const { heading, submitButton } = useStyles()
+
+  const [json, setJson] = useState<string>()
+
+  const onSubmit = (data: IFormInput) => {
+    setJson(JSON.stringify(data))
+  }
+
+  return (
+    <Container>
+      <Typography>Sign up</Typography>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <TextField
+          {...register('email')}
+          variant="outlined"
+          margin="normal"
+          label="Email"
+          fullWidth
+          required
+        />
+        <TextField
+          {...register('firstName')}
+          variant="outlined"
+          margin="normal"
+          label="First Name"
+          fullWidth
+          required
+        />
+        <TextField
+          {...register('password')}
+          variant="outlined"
+          margin="normal"
+          label="Password"
+          type="password"
+          fullWidth
+          required
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={submitButton}
+        >
+          Sign Up
+        </Button>
+      </form>
+    </Container>
+  )
+}
+
+export default App
