e0575c9d2d59154659b9676cf560f07e3da56f72
adriel3005 <30638262+adriel3005@users.noreply.github.com>
2023-02-21 21:27:31 -0600

    Retrieve Supabase DB Data (WIP)
    Ref #1

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './components/App/App'
 import reportWebVitals from './reportWebVitals'
 import CssBaseline from '@material-ui/core/CssBaseline'
+import { createClient } from '@supabase/supabase-js'
+import { Database } from '../src/types/forecasts'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
@@ -17,3 +19,23 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
+
+
+const supabase = createClient<Database>('url', 'key')
+
+export async function getForecasts() {
+  //return await supabase.from('forecasts').select()
+  const { data, error } = await supabase.from('forecasts').select('*')
+  console.log(data)
+  console.log(error)
+
+  console.log(await supabase.from('forecasts').select('*'))
+
+  //return { messages: data ?? [] }
+}
+getForecasts()
+//console.log(getForecasts())
+
+type forecastsResponse = Awaited<ReturnType<typeof getForecasts>>
+//export type forecastsResponseSuccess = forecastsResponse['data']
+//export type forecastsResponseError = forecastsResponse['error']
