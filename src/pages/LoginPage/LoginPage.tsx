7272e3dabb3f0078e11d78312c334d1b477beddd
adriel3005 <30638262+adriel3005@users.noreply.github.com>
2023-02-26 22:11:24 -0600

    Cleanup login and signup pages
    Ref #5

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,18 +1,10 @@
-import React from 'react'
-import {
-  makeStyles,
-  Container,
-  Typography,
-  TextField,
-  Button,
-} from '@material-ui/core'
 import InputForm, { InputType } from '../../components/InputForm/InputForm'
 
 const LoginPage = () => {
   return (
-    <Container>
+    <div>
       <InputForm inputType={InputType.Login} />
-    </Container>
+    </div>
   )
 }
 
--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -1,18 +1,10 @@
-import React from 'react'
-import {
-  makeStyles,
-  Container,
-  Typography,
-  TextField,
-  Button,
-} from '@material-ui/core'
 import InputForm, { InputType } from '../../components/InputForm/InputForm'
 
 const SignUpPage = () => {
   return (
-    <Container>
+    <div>
       <InputForm inputType={InputType.SignUp} />
-    </Container>
+    </div>
   )
 }
 
