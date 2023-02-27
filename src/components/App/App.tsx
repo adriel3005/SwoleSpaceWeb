0a4c4938dc3be959d1f0a17a1864aa486081806a
adriel3005 <30638262+adriel3005@users.noreply.github.com>
2023-02-26 22:12:43 -0600

    Add routing to landing page
    Add link to register in landing page
    
    Ref #5

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,26 @@
 // https://javascript.plainenglish.io/creating-a-sign-up-form-in-react-with-typescript-516b1a172913
 import './App.css'
 import LoginPage from '../../pages/LoginPage/LoginPage'
+import {
+  BrowserRouter,
+  Routes, //replaces "Switch" used till v5
+  Route,
+  Link,
+} from 'react-router-dom'
+import SignUpPage from '../../pages/SignUpPage/SignUpPage'
 
 function App() {
-  return <LoginPage />
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/Signup" element={<SignUpPage />} />
+        </Routes>
+        <Link to="/Signup">Sign Up</Link>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
