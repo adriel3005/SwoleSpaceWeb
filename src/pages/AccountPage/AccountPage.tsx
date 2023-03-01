63369e3c6b897b78a1d6ec60f31f538329f02a05
adriel3005 <30638262+adriel3005@users.noreply.github.com>
2023-02-28 22:20:08 -0600

    Add Account page
    Add routing to account page
    
    Ref #7

---
+++ src/pages/AccountPage/AccountPage.tsx
@@ -0,0 +1,12 @@
+import { Container, makeStyles, Typography } from '@material-ui/core'
+
+const AccountPage = () => {
+  return (
+    <Container>
+      <Typography>Account Page</Typography>
+      <p>Todo: Show sample DB data</p>
+    </Container>
+  )
+}
+
+export default AccountPage
