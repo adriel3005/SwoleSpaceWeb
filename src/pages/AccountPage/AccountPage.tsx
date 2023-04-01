c431b0ff4909695ed3290febd2d47e4404e9812b
adriel3005 <30638262+adriel3005@users.noreply.github.com>
2023-03-31 21:28:45 -0500

    Retrieve access token from user session
    Prevent user from accessing page without session
    
    Ref #13

--- src/pages/AccountPage/AccountPage.tsx
+++ src/pages/AccountPage/AccountPage.tsx
@@ -1,8 +1,9 @@
 import { Container, Typography } from '@material-ui/core'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
-
-import { supabase } from '../../supabase/SupabaseService'
+import { useSelector } from 'react-redux'
+import { Navigate, redirect } from 'react-router-dom'
+import { RootState } from '../../app/store'
 
 const AccountPage = () => {
   let dataSet:
@@ -16,29 +17,58 @@ const AccountPage = () => {
 
   const [weatherData, setWeatherData] = useState(dataSet)
   const [loading, setLoading] = useState(false)
+  const session = useSelector((state: RootState) => state.supabase.session)
 
   // Retrieve forecasts only once
   useEffect(() => {
     RenderForecasts()
   }, [])
 
+  // TODO: Find a way to store session in cache so it isn't lost on refresh
+
+  // Don't allow user to access this if session is null
+  if (session === null) {
+    console.log('null session: ' + session)
+    return <Navigate to="/" />
+  }
+
   async function RenderForecasts() {
     setLoading(true)
-    console.log('before axios')
-    axios.get(process.env.REACT_APP_SWOLE_BACKEND_URL!).then(response => {
-      console.log('got a response from axios')
-      console.log(response.data)
-    })
-    console.log('after axios')
 
-    // const { data, error } = await supabase.from('forecasts').select('*')
-    // // render data to user
-    // setWeatherData(data ?? [])
+    const config = {
+      headers: {},
+    }
+
+    axios
+      .get(process.env.REACT_APP_SWOLE_BACKEND_URL! + '/WeatherForecast', {
+        headers: { Authorization: session?.access_token },
+      })
+      .then(response => {
+        setWeatherData(response.data ?? [])
+      })
+      .catch(function (error) {
+        if (error.response.status === 401) {
+          console.log('Account Error: ' + error.response.data.message)
+
+          // TODO: send user back to login page
+        } else if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data)
+          alert(error.response.data.message)
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message)
+          alert('Error: ' + error.message)
+        }
+        console.log(error.config)
+      })
     setLoading(false)
   }
 
   return (
     <Container>
+      {}
       <Typography>Account Page</Typography>
       {loading && <p>Loading...</p>}
       <>
