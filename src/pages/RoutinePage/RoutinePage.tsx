6c4cc23fb2f67450f867a465bc09ca05d0a322e1
adriel3005 <30638262+adriel3005@users.noreply.github.com>
2023-04-14 23:35:45 -0500

    Add ability to create routine from exercise modal
    Ref #19

--- src/pages/RoutinePage/RoutinePage.tsx
+++ src/pages/RoutinePage/RoutinePage.tsx
@@ -7,6 +7,7 @@ import { RootState } from '../../app/store'
 import { Button, Typography } from '@material-ui/core'
 import { useEffect, useState } from 'react'
 import Modal from '../../components/modals/ExerciseModal/ExerciseModal'
+import { retrieveExercises } from '../../components/Services/Backend/SwoleBackend'
 
 const RoutinePage = () => {
   type Item = {
@@ -15,8 +16,16 @@ const RoutinePage = () => {
     sets: number
   }
 
-  let dataSet: Item[] | null = []
-  let modalDataSet: Item[] | null = []
+  type ExerciseItem = {
+    created_at: string
+    exercise_description: string | null
+    exercise_id: number
+    exercise_name: string
+    exercise_type: string
+  }
+
+  let dataSet: ExerciseItem[] | null = []
+  let modalDataSet: ExerciseItem[] | null = []
   const session = useSelector((state: RootState) => state.supabase.session)
   const [itemData, setItemData] = useState(dataSet)
   const [itemModalData, setItemModalData] = useState(modalDataSet)
@@ -29,14 +38,14 @@ const RoutinePage = () => {
     setisOpen(false)
   }
 
-  // TODO: look into why this sends to Account when signed in
-  //   if (session === null) {
-  //     console.log(session)
-  //     return <Navigate to="/" />
-  //   }
+  //TODO: look into why this sends to Account when signed in
+  if (session === null) {
+    console.log(session)
+    //return <Navigate to="/" />
+  }
 
   function AddItem(item: Item) {
-    setItemData(itemData.concat(item))
+    //setItemData(itemData.concat(item))
     closeModal()
   }
 
@@ -45,13 +54,24 @@ const RoutinePage = () => {
   // Add hardcoded item for now
   function AddModalItem() {
     let newItem: Item = { itemName: 'Item Name', repetitions: 3, sets: 3 }
-    setItemModalData(itemModalData.concat(newItem))
+    //setItemModalData(itemModalData.concat(newItem))
+
+    // TODO: Retrieve From backend and populate
+  }
+
+  async function PopulateModal() {
+    await retrieveExercises().then(response => {
+      console.log(response.data)
+      setItemModalData(response.data ?? [])
+    })
   }
 
   useEffect(() => {
     // Add 10 dummy items
-    if (itemModalData.length < 10) {
-      AddModalItem()
+    if (session !== null && itemModalData.length < 1) {
+      PopulateModal()
+      console.log('Modal was populated')
+      //AddModalItem()
     }
   })
 
@@ -75,10 +95,20 @@ const RoutinePage = () => {
           <div key={i}>
             Item {i}
             <br />
-            <p>Name: {element.itemName}</p>
-            <p>Reps: {element.repetitions}</p>
-            <p>Sets: {element.sets}</p>
-            <Button onClick={() => AddItem(element)}>Add Item</Button>
+            <p>created_at: {element.created_at}</p>
+            <p>exercise_description: {element.exercise_description}</p>
+            <p>exercise_id: {element.exercise_id}</p>
+            <p>exercise_name: {element.exercise_name}</p>
+            <p>exercise_type: {element.exercise_type}</p>
+            <Button
+              onClick={() => {
+                console.log(element)
+                setItemData(itemData.concat(element))
+                closeModal()
+              }}
+            >
+              Add Item
+            </Button>
             <hr />
           </div>
         ))}
@@ -89,9 +119,11 @@ const RoutinePage = () => {
         <div key={i}>
           Item {i}
           <br />
-          <p>Name: {element.itemName}</p>
-          <p>Reps: {element.repetitions}</p>
-          <p>Sets: {element.sets}</p>
+          <p>created_at: {element.created_at}</p>
+          <p>exercise_description: {element.exercise_description}</p>
+          <p>exercise_id: {element.exercise_id}</p>
+          <p>exercise_name: {element.exercise_name}</p>
+          <p>exercise_type: {element.exercise_type}</p>
           <hr />
         </div>
       ))}
