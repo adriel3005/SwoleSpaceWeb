9e63c09de9e30275a850841dbb3b5394c3814734
adriel3005 <30638262+adriel3005@users.noreply.github.com>
2023-04-26 19:39:40 -0500

    Add user routine page and populate with user routines
    Add backend call for DB retrieval
    
    Ref #24

--- src/components/Services/Backend/SwoleBackend.tsx
+++ src/components/Services/Backend/SwoleBackend.tsx
@@ -26,21 +26,57 @@ export async function retrieveExercises(): Promise<any> {
 // reID requires UUID
 export async function addRoutineExercise(
   reID: string,
+  urID: string,
   eID: string,
   rep: number,
   set: number,
-  userID: string
+  userID: string,
+  eO: number
 ): Promise<any> {
   return await axios.post(
     process.env.REACT_APP_SWOLE_BACKEND_URL! + '/AddRoutineExercise',
     {
       routine_exercise_id: reID,
+      user_routine_id: urID,
       exercise_id: eID,
       repetition: rep,
       sets: set,
       user_id: userID,
+      exercise_order: eO,
     },
     {
+      headers: {
+        Authorization: store.getState().supabase.session?.access_token,
+      },
+    }
+  )
+}
+
+// urID requires UUID
+// userID requires UUID
+export async function addUserRoutine(
+  urID: string,
+  userID: string
+): Promise<any> {
+  return await axios.post(
+    process.env.REACT_APP_SWOLE_BACKEND_URL! + '/AddUserRoutine',
+    {
+      user_routine_id: urID,
+      user_id: userID,
+    },
+    {
+      headers: {
+        Authorization: store.getState().supabase.session?.access_token,
+      },
+    }
+  )
+}
+
+export async function retrieveUserRoutines(userID: string): Promise<any> {
+  return await axios.get(
+    process.env.REACT_APP_SWOLE_BACKEND_URL! + '/GetUserRoutines',
+    {
+      params: { userID: userID },
       headers: {
         Authorization: store.getState().supabase.session?.access_token,
       },
---
+++ src/pages/UserRoutinePage/UserRoutinePage.tsx
@@ -0,0 +1,90 @@
+import { useSelector } from 'react-redux'
+import { RootState } from '../../app/store'
+import { Button, Typography } from '@material-ui/core'
+import { useEffect, useState } from 'react'
+import Modal from '../../components/modals/ExerciseModal/ExerciseModal'
+import {
+  addRoutineExercise,
+  retrieveExercises,
+  retrieveUserRoutines,
+} from '../../components/Services/Backend/SwoleBackend'
+import { v4 as uuidv4 } from 'uuid'
+import { Link } from 'react-router-dom'
+
+const UserRoutinePage = () => {
+  type ExerciseItem = {
+    created_at: string
+    exercise_description: string | null
+    exercise_id: string
+    exercise_name: string
+    exercise_type: string
+  }
+
+  type RoutineExercise = {
+    uuid: string
+    exercise: ExerciseItem
+    repetitions: number
+    sets: number
+  }
+
+  type Routines = {
+    user_routine_id: string
+    created_at: string
+  }
+
+  let dataSet: Routines[] | null = []
+  const session = useSelector((state: RootState) => state.supabase.session)
+  const [itemData, setItemData] = useState(dataSet)
+
+  //TODO: look into why this sends to Account when signed in
+  if (session === null) {
+    console.log(session)
+    //return <Navigate to="/" />
+  }
+
+  async function RetrieveUserRoutines() {
+    // TODO: retrieve user routine exercises created
+    await retrieveUserRoutines(session?.user.id!).then(response => {
+      console.log(response.data)
+      setItemData(response.data ?? [])
+    })
+  }
+
+  useEffect(() => {
+    // Populate Modal with DB items
+    if (session !== null /*&& itemModalData.length < 1*/) {
+      RetrieveUserRoutines()
+    }
+  })
+
+  return (
+    <div>
+      <Typography>User Routine Page</Typography>
+      {/* Add super generic item  */}
+      <div
+        style={{
+          color: 'inherit',
+          marginRight: 0,
+          marginLeft: 'auto',
+          alignItems: 'end',
+          display: 'table',
+        }}
+      >
+        <Button component={Link} to="/Routine">
+          Create New Routine
+        </Button>
+      </div>
+      {itemData?.map((element, i) => (
+        <div key={i}>
+          Routine {i}
+          <br />
+          <p>Routine ID: {element.user_routine_id}</p>
+          <p>Created At: {element.created_at}</p>
+          <hr />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default UserRoutinePage
