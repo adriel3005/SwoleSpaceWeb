7082490874075e2781b247fa6cdf9b6d03286dde
adriel3005 <30638262+adriel3005@users.noreply.github.com>
2023-05-09 22:40:21 -0500

    Update SQL script for get_routine_exercises function
    Ref #28

--- sql/fn_get_routine_exercises.sql
+++ sql/fn_get_routine_exercises.sql
@@ -3,6 +3,7 @@ RETURNS table (routine_exercise_id uuid,
   exercise_id int8,
   repetition int8,
   sets int8,
+  default_weight int8,
   user_id uuid,
   user_routine_id uuid,
   exercise_order int8
@@ -10,7 +11,7 @@ RETURNS table (routine_exercise_id uuid,
 AS $$
 DECLARE
 BEGIN
-    return query select ur."routine_exercise_id", ur."exercise_id", ur."repetition", ur."sets", ur."user_id", ur."user_routine_id", ur."exercise_order"
+    return query select ur."routine_exercise_id", ur."exercise_id", ur."repetition", ur."sets", ur."default_weight", ur."user_id", ur."user_routine_id", ur."exercise_order"
     From routine_exercise ur where  ur.user_routine_id = $1 order by ur.exercise_order ;
 END;
 $$ LANGUAGE plpgsql;
