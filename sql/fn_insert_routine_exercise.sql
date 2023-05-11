d3e87cb242f93835698773b0cdd697a8ebdbb312
adriel3005 <30638262+adriel3005@users.noreply.github.com>
2023-05-10 21:35:57 -0500

    Update insert routine exercise function
    Ref #28

--- sql/fn_insert_routine_exercise.sql
+++ sql/fn_insert_routine_exercise.sql
@@ -1,9 +1,9 @@
-CREATE OR REPLACE FUNCTION insert_routine_exercise(reID uuid, urID uuid, eID integer, rep integer, sets integer, userid uuid, eo integer)
+CREATE OR REPLACE FUNCTION insert_routine_exercise(reID uuid, urID uuid, eID integer, rep integer, sets integer, dw integer, userid uuid, eo integer)
   RETURNS void AS
   $BODY$
       BEGIN
-        INSERT INTO routine_exercise(routine_exercise_id, user_routine_id, exercise_id, repetition, sets, user_id, exercise_order)
-        VALUES(reID, urID, eID, rep, sets, userid, eo);
+        INSERT INTO routine_exercise(routine_exercise_id, user_routine_id, exercise_id, repetition, sets, default_weight, user_id, exercise_order)
+        VALUES(reID, urID, eID, rep, sets, dw, userid, eo);
       END;
   $BODY$
   LANGUAGE 'plpgsql' VOLATILE
